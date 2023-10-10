1d4a6f74e1bcea92b9a835aff190f38a34414355
DimasNuryanto <dimasnuryanto00@gmail.com>
2023-10-10 14:19:31 +0800

    commit kedua

---
+++ day2.java
@@ -0,0 +1,23 @@
+package harike2;
+public class day2 {
+    public static void main(String[] args) {
+        int intValue = 10;
+        float floatValue = 3.14f;
+        double doubleValue = 2.71828;
+        boolean booleanValue = true;
+        char charValue = 'A';
+        long longValue = 123456789;
+        short shortValue = 32000;
+        byte byteValue = 127;
+        
+        System.out.println("Nilai: " + intValue);
+        System.out.println("Nilai: " + floatValue);
+        System.out.println("Nilai: " + doubleValue);
+        System.out.println("Nilai: " + booleanValue);
+        System.out.println("Nilai: " + charValue);
+        System.out.println("Nilai: " + longValue);
+        System.out.println("Nilai: " + shortValue);
+        System.out.println("Nilai: " + byteValue);
+        
+    }
+}
