85cd2e82e1fff093d93febd6803b80890253ac6e
DimasNuryanto <147220926+DimasNuryanto@users.noreply.github.com>
2023-10-11 20:42:31 +0800

    Add files via upload

---
+++ day3.java
@@ -0,0 +1,19 @@
+
+package harike2;
+
+
+public class day3 {
+    public static void main(String[] args) {
+        String iniString ="kade";
+        Byte inibyte =19;
+        Short iniShort =2004;
+        Integer iniint =45;
+        Float inifloat =123f;
+        Double inidouble =1.71;
+        Character iniCharacter ='A';
+        Boolean iniboolean =false;
+        
+    }
+      
+    
+}
