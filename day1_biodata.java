5fe3f5c91349979f62c93f3ec0d0b0b3e456312a
DimasNuryanto <147220926+DimasNuryanto@users.noreply.github.com>
2023-10-12 15:27:02 +0800

    commit pertama

---
+++ day1_biodata.java
@@ -0,0 +1,18 @@
+package bismillah;
+public class day1_biodata {
+     public static void main(String[] args) {
+        String nama ="Dhimas Nuryanto";
+        String nim ="D0222548";
+        String kelas ="Informatika E";
+        String alamat ="Pangale";
+        int umur = 19;
+        int angkatan =2022;
+
+        System.out.println("NAMA     :"+ nama);
+        System.out.println("NIM      :"+ nim);
+        System.out.println("KELAS    :"+ kelas);
+        System.out.println("ALAMAT   :"+ alamat);
+        System.out.println("UMUR     :"+ umur);
+        System.out.println("ANGKATAN :"+ angkatan);
+    }
+}
