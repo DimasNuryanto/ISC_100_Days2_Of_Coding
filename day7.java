b7f327ab29dc26c2e1d391cac61dba517c0b8f1c
Dhimas Nuryanto <147220926+DimasNuryanto@users.noreply.github.com>
2023-10-15 18:24:51 +0800

    commit ke tujuh

---
+++ day7.java
@@ -0,0 +1,36 @@
+package harike2;
+public class day7 {
+    public static void main(String[] args) {
+        
+        System.out.println("b1 = 15");
+        System.out.println("b2 = 5");
+        int a = 15;
+        int b = 5;
+
+        System.out.println("tambah (a + b)");
+        int hasil = a + b;
+        System.out.println(a + b);
+
+        System.out.println("kurang (a - b)");
+        hasil = a - b;
+        System.out.println(a - b);
+
+        System.out.println("kali (a * b)");
+        hasil = a * b;
+        System.out.println(a * b);
+
+        System.out.println("c = 8");
+        System.out.println("d = 5");
+        float c = 8;
+        float d = 5;
+        
+        System.out.println("bagi (c / d)");
+        float hasilfloat = c / d;
+        System.out.println(c / d);
+
+        
+        System.out.println("sisa bagi (c % d)");
+        hasilfloat = c % d;
+        System.out.println(c % d);
+    }
+}
