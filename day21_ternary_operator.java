a615a8f0c788a2b3a3ec08d72d5de15f31f6b07d
Dhimas Nuryanto <147220926+DimasNuryanto@users.noreply.github.com>
2023-10-30 00:12:01 +0800

    commit ke dua puluh satu

---
+++ day21_ternary_operator.java
@@ -0,0 +1,15 @@
+package harike2;
+import java.util.Scanner;
+public class day21 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan angka: ");
+        int angka = sc.nextInt();
+
+        String hasil = (angka % 2 == 0) ? "Genap" : "Ganjil";
+        System.out.println("Angka ini adalah " + hasil);
+
+        sc.close();
+    }
+}
