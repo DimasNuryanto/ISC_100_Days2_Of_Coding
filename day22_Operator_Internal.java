dac8999a1ab9c998dbc8038e4a46a9e25ff2d61a
Dhimas Nuryanto <147220926+DimasNuryanto@users.noreply.github.com>
2023-10-30 15:52:52 +0800

    commit ke dua puluh dua

--- day22_Operator_Internal.java
+++ day22_Operator_Internal.java
@@ -4,17 +4,13 @@ public class day21 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("bila hasilnya ganjil akan bertambah angka 1 ");
-        System.out.println("bila hasilnya genap akan bertambah  angka 2 ");
-
-        System.out.print("Masukkan angka: ");
+        System.out.print("Enter the number : ");
         int angka = sc.nextInt();
         int hasil = (angka % 2 != 0) ? angka + 1 : angka + 2;
 
         String jenisBilangan = (hasil % 2 == 0) ? "genap" : "ganjil";
 
-        System.out.println("Hasilnya adalah: " + hasil);
-        System.out.println("merupakan bilangan " + jenisBilangan);
+        System.out.println(hasil + " bilangan " + jenisBilangan);
         sc.close();
     }
 }
