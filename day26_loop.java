87c5f06c7297234b823cb41f62a7994e3260cc8a
Dhimas Nuryanto <147220926+DimasNuryanto@users.noreply.github.com>
2023-11-03 20:45:21 +0800

    commit ke dua puluh enam

---
+++ day26_loop.java
@@ -0,0 +1,10 @@
+package harike2;
+public class day26 {
+    public static void main(String[] args) {
+        int angka = 1;
+        while (angka <= 5) {
+            System.out.println(angka);
+            angka++;
+        }
+    }
+}
