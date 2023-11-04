59ec38cd637a7bb0ecf59406e14b52956ed628f9
Dhimas Nuryanto <147220926+DimasNuryanto@users.noreply.github.com>
2023-11-04 21:29:24 +0800

    hari ke dua puluh tujuh

---
+++ day27_bintang.java
@@ -0,0 +1,12 @@
+package harike2;
+public class day28 {
+    public static void main(String[] args) {
+    int jumlahBaris = 4;
+        for (int i = 0; i < jumlahBaris; i++) {
+            for (int j = 0; j <= i; j++) {
+            System.out.print('*');
+            }
+        System.out.println();
+        }
+    }
+}
