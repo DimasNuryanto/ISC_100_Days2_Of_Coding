8fac87621a339b475fa5a5c42ab25ffbd1de32f7
Dhimas Nuryanto <147220926+DimasNuryanto@users.noreply.github.com>
2023-11-12 22:10:50 +0800

    commit ke tiga puluh lima

---
+++ day35_LoppingArray.java
@@ -0,0 +1,10 @@
+package harike2;
+public class day35 {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Elemen ke-" + i + ": " + numbers[i]);
+        }
+    }    
+}
