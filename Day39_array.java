a44881a6352582c7e93a4e6edc5c5b6a40ea307e
Dhimas Nuryanto <147220926+DimasNuryanto@users.noreply.github.com>
2023-11-16 23:06:15 +0800

    Commit ke tiga puluh sembilan

---
+++ Day39_array.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class day39_array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        System.out.print("[");
+
+        if (input % 2 == 0) {
+            
+            int[] arrayGenap = new int[input];
+            for (int i = 0; i < arrayGenap.length; i++) {
+                arrayGenap[i] = (i + 1) * 2;
+                
+            }
+
+            for (int num : arrayGenap) {
+                System.out.print(num + " ");  
+            }
+        } else {
+            
+            int[] arrayGanjil = new int[input];
+            for (int i = 0; i < arrayGanjil.length; i++) {
+                arrayGanjil[i] = i * 2 + 1;
+            }
+ 
+            for (int num : arrayGanjil) {
+                System.out.print(num + " ");
+           
+            }
+        }
+           System.out.print("]");
+    }
+}
