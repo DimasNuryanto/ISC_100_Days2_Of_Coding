0f0b1c5822e88622cb7e0e908429af0a37b39aec
Dhimas Nuryanto <147220926+DimasNuryanto@users.noreply.github.com>
2023-12-18 00:05:16 +0800

    commit ke tujuh puluh

---
+++ day70.java
@@ -0,0 +1,29 @@
+package harike2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class day70 {
+
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            System.out.print("Masukkan nama Anda: ");
+            String nama = reader.readLine();
+
+            System.out.print("Masukkan umur Anda: ");
+            int umur = Integer.parseInt(reader.readLine());
+
+            System.out.println("\nNama: " + nama);
+            System.out.println("Umur: " + umur);
+
+            reader.close();
+        } catch (IOException e) {
+            System.err.println("Error reading from user input: " + e.getMessage());
+        } catch (NumberFormatException e) {
+            System.err.println("Masukkan umur dengan format angka yang benar.");
+        }
+    }
+}
