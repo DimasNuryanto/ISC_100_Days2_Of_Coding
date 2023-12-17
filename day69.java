daeab199e16431dbf128ea18adaf6b5c70f7f4b3
Dhimas Nuryanto <147220926+DimasNuryanto@users.noreply.github.com>
2023-12-18 00:05:58 +0800

    commit ke enam puluh sembilan

---
+++ day69.java
@@ -0,0 +1,20 @@
+package harike2;
+
+public class day69 {
+
+    public static void main(String[] args) {
+        // Pengenalan String
+        String kata = "Halo, Saya adalah dhimas";
+
+        // Operasi pada String
+        System.out.println("Panjang kata: " + kata.length());
+        System.out.println("Huruf ke-7 dari kata: " + kata.charAt(8));
+
+        // Format String
+        String nama = "dhimas Nuryanto";
+        int umur = 19;
+        double tinggi = 175.5;
+        String formatString = String.format("Nama: %s, Umur: %d, Tinggi: %.2f", nama, umur, tinggi);
+        System.out.println("Format string: " + formatString);
+    }
+}
